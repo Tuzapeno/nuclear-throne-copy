cd53c47a3942f4ff3ca47f4446e7c42a43ef5816
Tuzapeno <tuzis6@gmail.com>
2024-10-07 15:15:09 -0300

    initial commit

---
+++ player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const SPEED: float = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	
+  var movement: Vector2 = Vector2()
+  movement.x = Input.get_axis("ui_left", "ui_right")
+  movement.y = Input.get_axis("ui_up", "ui_down")
+
+  movement = movement.normalized()
+
+  velocity = movement * SPEED
+  
+
+  move_and_slide()
