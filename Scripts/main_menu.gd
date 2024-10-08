c0f74dd31069db22df1bf5fdb52f48a8929a9aea
Tuzapeno <tuzis6@gmail.com>
2024-10-08 13:34:39 -0300

    feat: :sparkles: Create charater selection and scene manager
    Created character selection scene, and created scene manager to handle changing scenes with error check

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
+@onready var character_selection_scene: PackedScene = preload("res://Scenes/character_selection.tscn")
+
 # Signal for when the exit button is pressed
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
 # Signal for when the play button is pressed
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	SceneManager.change_scene(character_selection_scene, "Character Selection")
+
+	
---
+++ Scripts/scene_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func change_scene(scene: PackedScene, scene_name: String = "no name") -> void:
+	match get_tree().change_scene_to_packed(scene):
+		OK:
+			print("Scene changed successfully to ", scene_name)
+		ERR_CANT_CREATE:
+			print("Can't create scene ", scene_name)
+		ERR_INVALID_PARAMETER:
+			print("Scene ", scene_name, " is invalid!")
