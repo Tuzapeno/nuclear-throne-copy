7dad4d6af90d5d4618187024acf1de8b6b5afc8a
Tuzapeno <tuzis6@gmail.com>
2024-10-25 11:55:32 -0300

    feat: :lipstick: HUD ammo improvements

--- Scripts/UI/hud.gd
+++ Scripts/UI/hud.gd
@@ -4,6 +4,9 @@ extends CanvasLayer
 @onready var bullet_count: Label = $AmmoContainer/BulletCount
 @onready var bullet_bar: TextureProgressBar = $AmmoContainer/BulletProgressBar
 
+@onready var shell_count: Label = $AmmoContainer/ShellCount
+@onready var shell_bar: TextureProgressBar = $AmmoContainer/ShellProgressBar
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.game_started.connect(_on_game_start)
@@ -18,3 +21,6 @@ func _on_ammo_changed(type: int, value: int) -> void:
 		AmmoTypes.BULLET_TYPE:
 			bullet_count.text = str(value)
 			bullet_bar.value = value
+		AmmoTypes.SHELL_TYPE:
+			shell_count.text = str(value)
+			shell_bar.value = value
